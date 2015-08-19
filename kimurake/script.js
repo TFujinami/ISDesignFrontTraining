5853394f0582a2fee223f5c1fefb24ee5f868059
kimurake <kimurake@istyle.co.jp>
2015-08-19 18:52:48 +0900

    kimurake update

--- kimurake/script.js
+++ kimurake/script.js
@@ -1,1 +1,19 @@
+$(function(){
+	var jq_count = {
+		num: $('.count span:first-child'),
+		den: $('.count span:last-child'),
+		elem: $('.switch').children(),
+		cnt: $('.is-selected').length,
+		is_selected : 'is-selected',
+	}
+	$(jq_count.num).text(jq_count.cnt);
+	$(jq_count.den).text(jq_count.elem.length);
 
+	$(jq_count.elem).each(function(){
+		$(this).on('click', function(){
+			$(this).toggleClass(jq_count.is_selected);
+			var cnt = $('.is-selected').length;
+			$(jq_count.num).text(cnt);
+		});
+	});
+});
