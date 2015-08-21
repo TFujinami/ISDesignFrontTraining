ada4b64e1af480911a77c5a48965ef0cf2ed1711
yoshiia <yoshiia@istyle.co.jp>
2015-08-21 15:29:27 +0900

    yoshiia update

--- yoshiia/script.js
+++ yoshiia/script.js
@@ -1,1 +1,15 @@
+$(function(){
+  $(document).on('click','.switch a',function(e){
+    e.preventDefault();
+    $(this).parent().toggleClass('is-selected');
+    selectCounter();
+  });
 
+  selectCounter();
+
+  function selectCounter(){
+    var slctLen = $('.is-selected').length,
+        total = $('.switch').children().length;
+    $('.count').children().eq(0).html(slctLen).next().html(total);
+  }
+});
