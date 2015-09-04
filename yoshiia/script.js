99991eeb1a38822e6b8bfd204ae2e00640ab9521
yoshiia <yoshiia@istyle.co.jp>
2015-09-04 18:06:09 +0900

    update:ページ内複数個対応

--- yoshiia/script.js
+++ yoshiia/script.js
@@ -1,15 +1,36 @@
 $(function(){
-  $(document).on('click','.switch a',function(e){
+  var legacy = navigator.userAgent.match(/msie\s[0-9|10]+\.\d*/i);
+
+  if(legacy){
+    $(window).on('DOMNodeInserted', counterInit);
+  }else{
+    var mo = new MutationObserver(counterInit);
+  }
+
+  counterInit();
+
+  $(document).on('click','.switch-count a',function(e){
     e.preventDefault();
-    $(this).parent().toggleClass('is-selected');
-    selectCounter();
+    var $this = $(this),
+        $target = $this.parent(),
+        thisWrap = $this.closest('.switch-count')[0];
+    $target.toggleClass('is-selected');
+    counterUpdate(thisWrap);
   });
 
-  selectCounter();
+  function counterInit(){
+    legacy ? $(window).off('DOMNodeInserted', counterInit) : mo.disconnect();
+    $('.switch-count').each(function(){
+      counterUpdate(this);
+    });
+    legacy ? $(window).on('DOMNodeInserted', counterInit) : mo.observe(document,{childList:true, subtree:true});
+  };
 
-  function selectCounter(){
-    var slctLen = $('.is-selected').length,
-        total = $('.switch').children().length;
-    $('.count').children().eq(0).html(slctLen).next().html(total);
+  function counterUpdate(wrapper){
+    var $wrapper = $(wrapper),
+        $count = $wrapper.find('.count'),
+        slctLen = $wrapper.find('.is-selected').length,
+        total = $wrapper.find('.switch').children().length;
+    $count.children().eq(0).text(slctLen).next().text(total);
   }
 });
