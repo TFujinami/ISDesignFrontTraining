2fda9342cb55a9404c9bb7e25892ff9fb6b9eec2
kimurake <kimurake@istyle.co.jp>
2015-10-13 13:12:09 +0900

    update:複数対応

--- kimurake/script.js
+++ kimurake/script.js
@@ -1,19 +1,41 @@
 $(function(){
 	var jq_count = {
-		num: $('.count span:first-child'),
-		den: $('.count span:last-child'),
-		elem: $('.switch').children(),
-		cnt: $('.is-selected').length,
-		is_selected : 'is-selected',
+		root:         '.switch-count', // カウンタとスイッチを包括するクラス
+		count_area:   '.count',        // カウントエリアを包括するクラス
+		num:          '.num',          // 色のついた箱の数
+		total:        '.total',        // 箱の総数
+		elem_parent:  '.switch',       // 箱を包括するクラス
+		is_selected : 'is-selected',   // 色つきクラス
 	}
-	$(jq_count.num).text(jq_count.cnt);
-	$(jq_count.den).text(jq_count.elem.length);
 
-	$(jq_count.elem).each(function(){
-		$(this).on('click', function(){
-			$(this).toggleClass(jq_count.is_selected);
-			var cnt = $('.is-selected').length;
-			$(jq_count.num).text(cnt);
+	// 個数をはかるやつ
+	function countLength($target) {
+		return $target.length;
+	}
+
+	// カウンタとスイッチの固まりごとに実行してね
+	$(jq_count.root).each(function(){
+		var $root = $(this),									// 今いる固まり
+			$elem_parent = $root.find($(jq_count.elem_parent)),
+			$count_area = $root.find($(jq_count.count_area))
+			;
+
+		$(window).on('load', function(){
+			var total_count = countLength($elem_parent.children()),
+				num_count = countLength($elem_parent.children('.' + jq_count.is_selected));
+
+			$root.find($(jq_count.total)).text(total_count);
+			$root.find($(jq_count.num)).text(num_count);
+		});
+
+		var $element = $root.find(jq_count.elem_parent).children(); // 今いる固まりの中の箱を指定
+		// 今いる固まりの中の箱全部を対象に実行してね
+		$element.each(function(){
+			$(this).on('click', function(){
+				$(this).toggleClass(jq_count.is_selected);
+				var num_count = countLength($elem_parent.children('.' + jq_count.is_selected));
+				$root.find($(jq_count.num)).text(num_count);
+			});
 		});
 	});
 });
